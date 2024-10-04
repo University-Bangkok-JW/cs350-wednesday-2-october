083dff998e2559b742630b24752de8c8eee9e2e3
JetsadaWijit <jetsadawijit@outlook.com>
2024-10-04 08:17:43 +0700

    Added base code

--- 1640705339.py
+++ 1640705339.py
@@ -1,1 +1,28 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.nextNode = None
 
+class SLinkedList:
+    def __init__(self):
+        self.headNode = None
+
+    def printLinkedList(self):
+        cur = self.headNode
+        while cur is not None:
+            print(cur.data)
+            cur = cur.nextNode
+
+array = [1, 2, 3, 4, 5]
+list = SLinkedList()
+
+for data in array:
+    newNode = Node(data)
+    if list.headNode is None:
+        list.headNode = newNode
+        cur = list.headNode
+    else:
+        cur.nextNode = newNode
+        cur = cur.nextNode
+
+list.printLinkedList()
